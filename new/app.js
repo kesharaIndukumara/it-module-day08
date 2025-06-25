edff277a58572aa0f77eea9c0635493e302949b9
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-06-25 19:09:04 +0530

    array-sorting

--- new/app.js
+++ new/app.js
@@ -1,28 +1,65 @@
 console.log("Hi, Kesh");
 
 let customerList = [
-    "John Doe",
-    "Jane Smith",
-    "Alice Johnson",
-    "Bob Brown",
+    "x",
+    "Zamal",
+    "Xunil",
+    "amal",
+    "nimal",
     "Charlie Davis",
     "Emily Wilson",
+    "x"
 ];
-
-let customer = {
-    name: "John Doe",
-    age: 30,
-    email: "Joe@gmail.com"
-}
-
-customerList.push([10,20,30,40,50]);
 
 console.log(customerList);
 
+
+// let customer = {
+//     name: "John Doe",
+//     age: 30,
+//     email: "Joe@gmail.com"
+// }
+
+// customerList.push([10,20,30,40,50]);
+
+// console.log(customerList);
+
 // customerList.pop();
 // customerList.pop();
 
-customerList.shift();// Removes the first element from the array
-customerList.unshift("New Customer"); // Adds a new element at the beginning of the array
+// customerList.shift();// Removes the first element from the array
+// customerList.unshift("New Customer"); // Adds a new element at the beginning of the array
 
+// let index = customerList.indexOf("nimal");
+// console.log(index);
+
+// console.log(customerList[index] = "bimal");
+// console.log(customerList);
+
+// console.log(customerList.includes("kamal")); // Checks if "kamal" is in the array
+
+let letters = [
+    "F",
+    "E",
+    "C",
+    "D",
+    "B",
+    "A"
+];
+
+// console.log(letters);
+
+// console.log(letters.sort()); 
+
+// console.log(customerList.sort());
+
+let numbers = [
+    4,
+    2, 
+    5,
+    1
+]
+// console.log(numbers);
+
+// console.log(numbers.sort());
 
