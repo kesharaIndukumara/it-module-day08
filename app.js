a02c3a4c7667fcd5cc2e56f8f54f9f5a12a9b3f0
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-06-25 17:32:02 +0530

    set-count-variable

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
 // const number = 20;
 let number = Math.floor(Math.random() * 10);
-console.log
+console.log(number);
 
 function resetNumber(){
     number = Math.floor((Math.random() * 10)+1);
+    console.log(number);
 }
+
+let count = 3;
 
 function isCorrectGuess(num) {
     num = parseInt(num);
-
     if(num == number){
         document.getElementById("txt-number").value = "";
         resetNumber()
+        count--;
+        if(count > 0){
+            document.getElementById("count").innerHTML = count;
+        }else{
+            document.getElementById("btn-guess").disabled = true;
+        }
         return "Congratulations! You guessed the correct number.";
     }else if(num < number){
+        count--;
+        if(count > 0){
+            document.getElementById("count").innerHTML = count;
+        }else{
+            document.getElementById("btn-guess").disabled = true;
+        }
         document.getElementById("txt-number").value = "";
         return "Your guess is too low. Try again!";
     }else if(num > number){
+        count--;
+        if(count > 0){
+            
+            document.getElementById("count").innerHTML = count;
+        }else{
+            document.getElementById("btn-guess").disabled = true;
+        }
         document.getElementById("txt-number").value = "";
         return "Your guess is too high. Try again!";
     }else{
+        count--;
+        if(count > 0){
+            
+            document.getElementById("count").innerHTML = count;
+        }else{
+            document.getElementById("btn-guess").disabled = true;
+        }
         document.getElementById("txt-number").value = "";
         return "Try Again..";
     }
