a0b865c16dcd233ae03efd2d8b66b0c4452c0171
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-06-25 18:18:37 +0530

    initial commit: add app.js and index.html with customer list and logging

---
+++ new/app.js
@@ -0,0 +1,20 @@
+console.log("Hi, Kesh");
+
+let customerList = [
+    "John Doe",
+    "Jane Smith",
+    "Alice Johnson",
+    "Bob Brown",
+    "Charlie Davis",
+    "Emily Wilson",
+];
+
+let customer = {
+    name: "John Doe",
+    age: 30,
+    email: "Joe@gmail.com"
+}
+
+customerList.push([10,20,30,40,50]);
+
+console.log(customerList);
